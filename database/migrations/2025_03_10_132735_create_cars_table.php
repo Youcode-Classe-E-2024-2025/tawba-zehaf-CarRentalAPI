132842e230c8f69deedc77d5f919c27cd7e390f9
tawbazf <tawbazehaf@gmail.com>
2025-03-10 14:29:23 +0100

    cars table

--- database/migrations/2025_03_10_132735_create_cars_table.php
+++ database/migrations/2025_03_10_132735_create_cars_table.php
@@ -13,10 +13,6 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('model');
-            $table->string('color');
-            $table->string('price');
             $table->timestamps();
         });
     }
