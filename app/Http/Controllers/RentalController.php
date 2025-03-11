4e6b856ffeb5c26ec68962122d37da28016e5076
tawbazf <tawbazehaf@gmail.com>
2025-03-11 11:00:57 +0100

    rentalcontroller and his functions

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rental;
 use Illuminate\Http\Request;
 
 class RentalController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Rental::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Rental $rental)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'car_id' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+        ]);
+
+        $rental = Rental::create([
+            'user_id' => $request->user_id,
+            'car_id' => $request->car_id,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date
+        ]);
+
+        return response()->json([
+            'message' => 'Rental created successfully',
+            'rental' => $rental
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rental $rental)
+    {
+        return $rental;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Rental $rental)
+    {
+        $rental->update($request->all());
+        return $rental;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rental $rental)
+    {
+        $rental->delete();
+        return response()->json([
+            'message' => 'Rental deleted successfully',
+            'rental' => $rental
+        ]);
+    }
 }
