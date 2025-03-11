b88f73ea4b760aaaab598131999ab3f699da5217
tawbazf <tawbazehaf@gmail.com>
2025-03-11 11:08:37 +0100

    api.route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::apiResource('users', AuthController::class);
+
+Route::apiResource('cars', CarController::class)->middleware('auth:sanctum');
+
+Route::apiResource('rentals', RentalController::class)->middleware('auth:sanctum');
+
+Route::apiResource('payments', PaymentController::class)->middleware('auth:sanctum');
